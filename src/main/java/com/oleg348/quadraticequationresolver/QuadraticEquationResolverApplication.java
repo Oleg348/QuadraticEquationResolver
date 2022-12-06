53c2a4887dfe11a58d784d3b672257bcd8bc3c72
Oleg348 <zherdev96@gmail.com>
2022-12-06 13:28:59 +0300

    initial commit

---
+++ src/main/java/com/oleg348/quadraticequationresolver/QuadraticEquationResolverApplication.java
@@ -0,0 +1,12 @@
+package com.oleg348.quadraticequationresolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuadraticEquationResolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuadraticEquationResolverApplication.class, args);
+	}
+}
---
+++ src/test/java/com/oleg348/quadraticequationresolver/QuadraticequationresolverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oleg348.quadraticequationresolver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuadraticequationresolverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
